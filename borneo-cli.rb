0512f71a8105ebe843783ad887d8d85a937e23f4
Jan Hecking <jan@borneo.io>
2023-10-06 18:21:55 +0800

    Bump borneo-cli to v3.8.0-alpha.3

--- borneo-cli.rb
+++ borneo-cli.rb
@@ -4,16 +4,16 @@
 class BorneoCli < Formula
   desc "Command Line Interface (CLI) for the Borneo API"
   homepage "https://www.borneo.io/"
-  version "3.8.0-alpha.2"
+  version "3.8.0-alpha.3"
   version_scheme 1
 
-  url "https://github.com/borneodata/homebrew-tap/releases/download/v3.8.0-alpha.2/borneo-cli-v3.8.0-alpha.2-ed1eec20b-darwin-arm64.tar.gz"
-  sha256 "1795badaa4db9cafa4be1a47234f8423aa1341c76b78675095b87ee07fa57373"
+  url "https://github.com/borneodata/homebrew-tap/releases/download/v3.8.0-alpha.3/borneo-cli-v3.8.0-alpha.3-bc4953973-darwin-arm64.tar.gz"
+  sha256 "ee7b6f39e0361a438097941b10c927ac58dd7e39637b44a76b3b1f206aaffced"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/borneodata/homebrew-tap/releases/download/v3.8.0-alpha.2/borneo-cli-v3.8.0-alpha.2-ed1eec20b-darwin-x64.tar.gz"
-      sha256 "c148052eba59a78e195859f9b467b627b745eb6d5044a8567c89591f569fa3c9"
+      url "https://github.com/borneodata/homebrew-tap/releases/download/v3.8.0-alpha.3/borneo-cli-v3.8.0-alpha.3-bc4953973-darwin-x64.tar.gz"
+      sha256 "736984bb407866c3b822f3e2057b299312176e7d95137d992a1a13b8406c3a4a"
     end
   end
 
