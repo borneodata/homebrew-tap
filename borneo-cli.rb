4541f65159d3c62cca99137dd88200c921045e29
Jan Hecking <jan@borneo.io>
2023-10-09 19:24:12 +0800

    Fix checksums for borneo-cli@alpha.4

--- borneo-cli.rb
+++ borneo-cli.rb
@@ -8,12 +8,12 @@ class BorneoCli < Formula
   version_scheme 1
 
   url "https://github.com/borneodata/homebrew-tap/releases/download/v3.8.0-alpha.4/borneo-cli-v3.8.0-alpha.4-2d62f9a7d-darwin-arm64.tar.gz"
-  sha256 "187442696ce6c317db578f243fd801af88b6593aa70561f860a5b7cbb2579c6b"
+  sha256 "e9a2a3e7ec307787927ec7b21c0de40b88aba477e056a3fc702773c2d141eedd"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/borneodata/homebrew-tap/releases/download/v3.8.0-alpha.4/borneo-cli-v3.8.0-alpha.4-2d62f9a7d-darwin-x64.tar.gz"
-      sha256 "ca3b7027cfc4376728fcbe0ed5a80f5111fa85c7d266f655bbfdbaa7d2e985b9"
+      sha256 "7ea8a5b5ff29c98b4060aec4371496e5a55c2c71bab672f255ec94a83a69716b"
     end
   end
 
