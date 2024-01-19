c3cdd6d1a101d6b7bf1accf9f99fe99bd0fb8061
Jeet Kumar Prajapati <141103688+jeet-borneo@users.noreply.github.com>
2024-01-19 15:55:29 +0530

    Release borneo cli 3.9.1 (#7)
    * release borneo-cli-3.9.1
    
    * add correct version
    
    * remove -cli from borneo-cli version

--- borneo-cli.rb
+++ borneo-cli.rb
@@ -4,16 +4,16 @@
 class BorneoCli < Formula
   desc "Command Line Interface (CLI) for the Borneo API"
   homepage "https://www.borneo.io/"
-  version "3.9.0-cli"
+  version "3.9.1"
   version_scheme 1
 
-  url "https://github.com/borneodata/homebrew-tap/releases/download/v3.9.0-cli/borneo-v3.9.0-075bb970c-darwin-arm64.tar.gz"
-  sha256 "0deecc23e82fbe259d5f1098ac5ed87b7435fbdc0c102d9395d61ac4aa5181bb"
+  url "https://github.com/borneodata/homebrew-tap/releases/download/v3.9.1/borneo-v3.9.1-b23678aa3-darwin-arm64.tar.gz"
+  sha256 "8412428126bd3a685d6980dcc79d0c09dd58c9199074c2ae750a3bc1077a8fd1"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/borneodata/homebrew-tap/releases/download/v3.9.0-cli/borneo-v3.9.0-075bb970c-darwin-x64.tar.gz"
-      sha256 "176467254940b79c6ee0cb602e2b7ea1b4cec727edc8c657a8ded29b7ead42de"
+      url "https://github.com/borneodata/homebrew-tap/releases/download/v3.9.1/borneo-v3.9.1-b23678aa3-darwin-x64.tar.gz"
+      sha256 "c510a65c9330bf9c9dad4b296ab4186cb0891266676b642056cbeda1755e10f8"
     end
   end
 
