9775217b582f7546b8d437af98f97e89147e8725
Manan <manan@borneo.io>
2024-02-18 22:50:11 +0530

    use variables instead of hardcoding for urls and sha so that they can be changed from borneo cli release workflow

--- borneo-cli.rb
+++ borneo-cli.rb
@@ -7,13 +7,18 @@ class BorneoCli < Formula
   version "3.9.1"
   version_scheme 1
 
-  url "https://github.com/borneodata/homebrew-tap/releases/download/v3.9.1/borneo-v3.9.1-b23678aa3-darwin-arm64.tar.gz"
-  sha256 "8412428126bd3a685d6980dcc79d0c09dd58c9199074c2ae750a3bc1077a8fd1"
+  URL_ARM="https://github.com/borneodata/homebrew-tap/releases/download/v3.9.1/borneo-v3.9.1-b23678aa3-darwin-arm64.tar.gz"
+  SHA_ARM="8412428126bd3a685d6980dcc79d0c09dd58c9199074c2ae750a3bc1077a8fd1"
+  URL_X64="https://github.com/borneodata/homebrew-tap/releases/download/v3.9.1/borneo-v3.9.1-b23678aa3-darwin-x64.tar.gz"
+  SHA_X64="c510a65c9330bf9c9dad4b296ab4186cb0891266676b642056cbeda1755e10f8"
+
+  url URL_ARM
+  sha256 SHA_ARM
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/borneodata/homebrew-tap/releases/download/v3.9.1/borneo-v3.9.1-b23678aa3-darwin-x64.tar.gz"
-      sha256 "c510a65c9330bf9c9dad4b296ab4186cb0891266676b642056cbeda1755e10f8"
+      url URL_X64
+      sha256 SHA_X64
     end
   end
 
