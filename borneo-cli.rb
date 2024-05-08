f14664a88392e9e7f086f9ce7f150635f0a8296b
securezapp-machine-user <admin@borneo.io>
2024-05-08 10:55:23 +0000

    Update Borneo formula with new URL and SHA

--- borneo-cli.rb
+++ borneo-cli.rb
@@ -4,13 +4,13 @@
 class BorneoCli < Formula
   desc "Command Line Interface (CLI) for the Borneo API"
   homepage "https://www.borneo.io/"
-  version "v3.10.5"
+  version "v3.11.6"
   version_scheme 1
 
-  URL_ARM="https://github.com/borneodata/homebrew-tap/releases/download/borneo-v3.10.5-cli/borneo-v3.10.0-5060ac2-darwin-arm64.tar.gz"
-  SHA_ARM="ef96c324614cf4769485160a48d0ee9972a0ef97ae3c1920bd0b0dc03451b9c6"
-  URL_X64="https://github.com/borneodata/homebrew-tap/releases/download/borneo-v3.10.5-cli/borneo-v3.10.0-5060ac2-darwin-x64.tar.gz"
-  SHA_X64="611300db571b29c62b145e3873631fdac2504e5600655c542678fed8bc006426"
+  URL_ARM="https://github.com/borneodata/homebrew-tap/releases/download/borneo-v3.11.6-cli/borneo-v3.10.0-8aa79cd-darwin-arm64.tar.gz"
+  SHA_ARM="16af3cd39c485eb955a13771742a074ec1094d0816d893ad201b1774ed69ff38"
+  URL_X64="https://github.com/borneodata/homebrew-tap/releases/download/borneo-v3.11.6-cli/borneo-v3.10.0-8aa79cd-darwin-x64.tar.gz"
+  SHA_X64="eb09ad0b611704f6a6215853744954315c99a57368fce22264973f58fdf30eed"
 
   url URL_ARM
   sha256 SHA_ARM
